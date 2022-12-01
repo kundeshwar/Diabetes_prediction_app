953250e0106c83d1b67e77798767887f095a44af
KUNDESHWAR VIJAYRAO PUNDALIK <110928245+kundeshwar@users.noreply.github.com>
2022-12-01 12:12:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pickle
+import streamlit as st
+
+loading_model = pickle.load(open("C:/Users/kunde/all vs code/ml prject/web_page_of_diabeties/trained_model.pkl", 'rb'))
+
+
+def diabetes_pred(input_data):
+    #to will giv e reshape
+    x_arra = np.asarray(input_data)
+    x_reshape = x_arra.reshape(1, -1)
+    predt_y = loading_model.predict(x_reshape)
+    print(predt_y)
+    if predt_y[0] == 0:
+        return "The person is not diabetes"
+    else:
+        return "The person is diabetes"
+
+
+#using and creating stramlit
+
+def main():
+
+    #giving title
+    st.title("DIABETES PREDICTION")
+
+    #giting input values 
+    Pregnancies = st.text_input("Number of Pregnancies")
+    Glucose = st.text_input("Number of Glucose")
+    BloodPressure = st.text_input("BloodPressure Level")
+    SkinThickness = st.text_input("SkinThickness values ")
+    Insulin = st.text_input("Insulin Level")
+    BMI_i = st.text_input("BMI(body mass index)")
+    DiabetesPedigreeFunction = st.text_input("DiabetesPedigreeFunction value")
+    age = st.text_input("Age of you")
+
+    # code of prediction 
+    diagnosis = ""
+
+    #creating button
+    if st.button("Diabetes Test Result"):
+        diagnosis = diabetes_pred([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI_i, DiabetesPedigreeFunction, age])
+    
+    st.success(diagnosis)
+
+
+
+if __name__=='__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
